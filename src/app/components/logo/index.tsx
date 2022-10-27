9616cbd19ad442b5aa923ffb40387bbe206d07b0
Emad Baqeri <ebaqeri@protonmail.com>
2022-10-27 14:38:12 +0330

    Bugfix for PR #1
    PR_#1: Import/export pattern was not implemented correctly under the
    components directory
    Updated the folder namespaces and implemented the import/export pattern
    to resolve the issues.

--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -1,1 +1,10 @@
-export * from './Logo'
+import logo from "../../../assets/logo.svg";
+
+interface LogoProps {
+  height?: number;
+  width?: number;
+}
+
+export const Logo: React.FC<LogoProps> = ({ height = 40, width = 40 }) => {
+  return <img src={logo} height={height} width={width} alt="testfully logo" />;
+};
